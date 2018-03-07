a2dff190dc1738a5624112976bb2add0e054cbfa
Nicholas Kostelnik <nkostelnik@gmail.com>
2018-03-07 11:34:31 +0800

    kind of works

--- audio_data.py
+++ audio_data.py
@@ -27,60 +27,6 @@ else:
 
 fingerprint_size = dct_coefficient_count * spectrogram_length
 
-class Datasets:
-  def __init__(self, train, valid, test):
-    self.train = train
-    self.valid = valid
-    self.test = test
-
-class Dataset:
-  def __init__(self, files):
-    self.files = files
-    self.num_examples = len(files)
-
-  def next_batch(self, offset, batch_size, sess):
-    data = np.zeros((batch_size, fingerprint_size))
-    labels = np.zeros((batch_size, 31))
-
-    # print(batch_size * offset, batch_size * offset + batch_size)
-
-    for i, j in enumerate(range(batch_size * offset, batch_size * offset + batch_size)):
-      input_filename = tf.placeholder(tf.string, [])
-      wav_loader = io_ops.read_file(input_filename)
-      file = self.files[j]
-
-      wav_decoder = contrib_audio.decode_wav(
-        wav_loader,
-        desired_channels=desired_channels,
-        desired_samples=sample_rate
-      )
-
-      spectrogram = contrib_audio.audio_spectrogram(
-        wav_decoder.audio,
-        window_size_samples,
-        window_stride_samples
-      )
-
-      mfcc = contrib_audio.mfcc(
-        spectrogram,
-        sample_rate=wav_decoder.sample_rate,
-        dct_coefficient_count=dct_coefficient_count
-      )
-
-      feed_dict = {
-        input_filename: file['path']
-      }
-
-      data[i, :] = sess.run(mfcc, feed_dict).flatten()
-
-      # We might not want to straight up return this
-      labels[i, :] = file['label']
-
-    return [
-      data,
-      labels
-    ]
-
 
 MAX_NUM_WAVS_PER_CLASS = 2**27 - 1 # ~134M
 
@@ -100,16 +46,60 @@ def which_set(filename, validation_percentage, testing_percentage):
 
   return result
 
+def encode_image(image_filepath):
+  print("Encoding", image_filepath)
+  with tf.Session() as sess:
+    input_filename = tf.placeholder(tf.string, [])
+    wav_loader = io_ops.read_file(input_filename)
+
+    wav_decoder = contrib_audio.decode_wav(
+      wav_loader,
+      desired_channels=desired_channels,
+      desired_samples=sample_rate
+    )
+
+    spectrogram = contrib_audio.audio_spectrogram(
+      wav_decoder.audio,
+      window_size_samples,
+      window_stride_samples
+    )
+
+    mfcc = contrib_audio.mfcc(
+      spectrogram,
+      sample_rate=wav_decoder.sample_rate,
+      dct_coefficient_count=dct_coefficient_count
+    )
+
+    feed_dict = {
+      input_filename: image_filepath
+    }
+
+    encoded_data = sess.run(mfcc, feed_dict).flatten()
+    return encoded_data
+
+class Datasets:
+  def __init__(self, train, valid, test):
+    self.train = train
+    self.valid = valid
+    self.test = test
+
+class Dataset:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
 def load_data(path):
   files_path = os.path.join(path, '**/*.wav')
   files = glob.glob(files_path)
   np.random.shuffle(files)
 
-  buckets = {
-    'training':   [],
-    'validation': [],
-    'testing':    []
-  }
+  files = files[0:1000]
+
+  # buckets = {
+  #   'training':   [],
+  #   'validation': [],
+  #   'testing':    []
+  # }
 
   label_buckets = {}
   for file in files:
@@ -122,20 +112,27 @@ def load_data(path):
   int_to_label = dict((i, c) for i, c in enumerate(labels))
 
   one_hot = list(i for i, c in enumerate(labels))
-  int_to_onehot = np.eye(len(one_hot))
+  int_to_onehot = np.eye(len(one_hot), dtype=int)
+
+  training_x = []
+  training_y = []
 
   for file in files:
     set = which_set(file, 20, 20)
     label = file.split(os.sep)[-2]
 
     label_index = label_to_int[label]
-    label_one_hot = int_to_onehot[label_index]
+    # label_one_hot = int_to_onehot[label_index]
 
-    sample = { 'label': label_one_hot, 'path': file }
-    buckets[set].append(sample)
+    encoded_data = encode_image(file)
+    training_x.append(encoded_data)
+    training_y.append(label_index)
 
-  training   = Dataset(buckets['training'][:300])
-  validation = Dataset(buckets['validation'][:300])
-  testing    = Dataset(buckets['testing'][:300])
+  train_x = np.array(training_x)
+  train_y = np.array(training_y, dtype=int)
 
-  return Datasets(training, validation, testing)
+  training   = Dataset(train_x, train_y)
+  # validation = Dataset(buckets['validation'][:300])
+  # testing    = Dataset(buckets['testing'][:300])
+
+  return Datasets(training, None, None)
--- main4.py
+++ main4.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
+import numpy as np
 from keras.models import Sequential
+from keras.utils import to_categorical
+
 
 model = Sequential()
 
@@ -9,14 +12,18 @@ from audio_data import load_data
 
 cmds = load_data('/home/nk/Development/scratch/speech_commands')
 
-model.add(Dense(31, input_shape=(3920,)))
+y_train = to_categorical(cmds.train.y)
+x_train = cmds.train.x
+num_classes = y_train.shape[1]
+
+model.add(Dense(num_classes, input_shape=(3920,)))
+model.add(Dense(num_classes, input_shape=(3920,)))
+model.add(Dense(num_classes, input_shape=(3920,)))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-cmds = load_data('/home/nk/Development/scratch/speech_commands')
+print(x_train.shape)
 batch_size = 100
-step = 0
 
-with tf.Session() as sess:
-  x_train, y_train = cmds.train.next_batch(step, batch_size, sess)
+# model.train_on_batch(x_batch, y_batch)
 
-  model.fit(x_train, y_train, epochs=5, batch_size=batch_size)
+model.fit(x_train, y_train, epochs=500, batch_size=batch_size)
