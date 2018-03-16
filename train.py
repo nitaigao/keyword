9a5683411be061d73b58966b0f2f6e9dccdece02
Nicholas Kostelnik <nkostelnik@gmail.com>
2018-03-16 18:35:42 +0800

    fixing linting issues

--- train.py
+++ train.py
@@ -1,59 +1,69 @@
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from keras.layers import Dense, Conv2D, Dropout, MaxPooling2D, Flatten
 from keras.utils import np_utils
 from keras.callbacks import Callback
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import confusion_matrix
 import numpy as np
 
 from audio_data import load_data
 
-model = Sequential()
+NUM_CLASSES = 15
 
-num_classes = 15
-
-path = '/home/nk/Development/scratch/speech_commands'
-cmds = load_data(path)
+CMDS = load_data('/home/nk/Development/scratch/speech_commands')
 
 class Callbacks(Callback):
-  def on_epoch_end(self, epoch, logs={}):
-    x, y = cmds.test.fetch_batch(0, 100)
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def on_epoch_end(self, epoch, logs):
+        x, y = CMDS.test.fetch_batch(0, 100)
+        x_test = x.reshape(-1, 98, 40, 1)
+        y_test = np_utils.to_categorical(y, NUM_CLASSES)
+
+        predictions = self.model.predict(x_test, batch_size=100)
+        classes = np.argmax(predictions, axis=1)
+
+        print(confusion_matrix(y, classes))
+        return
+
+def train_generator(batch_size, steps):
+    step = 0
+    while 1:
+        if steps == step:
+            step = 0
+
+        x, y = CMDS.train.fetch_batch(step, batch_size)
+        x = x.reshape(-1, 98, 40, 1)
+        y = np_utils.to_categorical(y, NUM_CLASSES)
+        yield (x, y)
+        step += 1
+
+def main():
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(98, 40, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dropout(0.4))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+    model.summary()
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    epochs = 200
+    batch_size = 100
+    steps = 20000 / batch_size
+    callbacks = Callbacks(model)
+    model.fit_generator(train_generator(batch_size, steps), steps_per_epoch=steps,
+                                                            epochs=epochs,
+                                                            callbacks=[callbacks])
+
+    x, y = CMDS.test.fetch_batch(0, 100)
     x_test = x.reshape(-1, 98, 40, 1)
-    y_test = np_utils.to_categorical(y, num_classes)
+    y_test = np_utils.to_categorical(y, NUM_CLASSES)
 
     predictions = model.predict(x_test, batch_size=100)
     classes = np.argmax(predictions, axis=1)
 
     print(confusion_matrix(y, classes))
-    return
-
-def train_generator(batch_size, steps):
-  step = 0
-  while 1:
-    if steps == step:
-      step = 0
-
-    x, y = cmds.train.fetch_batch(step, batch_size)
-    x = x.reshape(-1, 98, 40, 1)
-    y = np_utils.to_categorical(y, num_classes)
-    yield (x, y)
-    step += 1
-
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(98, 40, 1)))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Flatten())
-model.add(Dense(num_classes, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-batch_size = 100
-steps = 20000 / batch_size
-callbacks = Callbacks()
-model.fit_generator(train_generator(batch_size, steps), steps_per_epoch=steps, epochs=10, callbacks=[callbacks])
-
-x, y = cmds.test.fetch_batch(0, 100)
-x_test = x.reshape(-1, 98, 40, 1)
-y_test = np_utils.to_categorical(y, num_classes)
-
-predictions = model.predict(x_test, batch_size=100)
-classes = np.argmax(predictions, axis=1)
-
-print(confusion_matrix(y, classes))
